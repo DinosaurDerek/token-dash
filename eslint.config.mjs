dc08dc358a8fe877a351ebc44ef6610c1035d030
DinosaurDerek <derekjpaul@gmail.com>
2025-12-08 16:46:57 -0800

    updates next.js/react to patched versions and updates eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// eslint.config.mjs
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+export default defineConfig([
+  ...nextVitals,
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  {
+    rules: {
+      // These patterns are intentionally used in this codebase
+      "react/display-name": "off",
+      "react-hooks/set-state-in-effect": "off",
+    },
+  },
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+]);
